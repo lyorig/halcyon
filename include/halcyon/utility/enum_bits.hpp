f955939c839fea1d8343436a67676bc8ee265850
lyorig <31075205+lyorig@users.noreply.github.com>
2024-07-22 02:32:52 +0200

    Removed enum bitset

--- include/halcyon/utility/enum_bits.hpp
+++ include/halcyon/utility/enum_bits.hpp
@@ -11,43 +11,12 @@
 
 namespace hal
 {
-    namespace detail
-    {
-        template <typename Enum, typename Value>
-            requires std::is_enum_v<Enum>
-        class enum_bit_base
-        {
-        public:
-            constexpr enum_bit_base()
-                : m_mask { 0 }
-            {
-            }
-
-            constexpr enum_bit_base(Value val)
-                : m_mask { val }
-            {
-            }
-
-            constexpr Value mask() const
-            {
-                return m_mask;
-            }
-
-            constexpr bool operator<=>(const enum_bit_base&) const = default;
-
-        protected:
-            Value m_mask;
-        };
-    }
-
     // A bitset-like class for (scoped) enums defined as bit masks (i.e. 0b1, 0b10, 0b100...).
     template <typename Enum, typename Value = meta::underlying_type<Enum>>
         requires std::is_enum_v<Enum>
-    class enum_bitmask : public detail::enum_bit_base<Enum, Value>
+    class enum_bitmask
     {
     private:
-        using super = detail::enum_bit_base<Enum, Value>;
-
         constexpr static Value reduce(std::initializer_list<Enum> e)
         {
             Value mask { 0 };
@@ -59,96 +28,79 @@ namespace hal
         }
 
     public:
-        using super::super;
+        // Default constructor, initializes to zero.
+        constexpr enum_bitmask()
+            : m_mask { 0 }
+        {
+        }
 
+        // Construct from an integral value.
+        // This is mostly used by Halcyon internally.
+        constexpr enum_bitmask(Value v)
+            : m_mask { v }
+        {
+        }
+
+        // Construct from a single enum.
         constexpr enum_bitmask(Enum e)
             : enum_bitmask { e }
         {
         }
 
+        // OR together multiple enums.
         constexpr enum_bitmask(std::initializer_list<Enum> il)
-            : super { reduce(il) }
+            : enum_bitmask { reduce(il) }
         {
+        }
+
+        constexpr Value mask() const
+        {
+            return m_mask;
+        }
+
+        // Bitwise operators.
+
+        constexpr enum_bitmask operator&(enum_bitmask b) const
+        {
+            return mask() & b.mask();
+        }
+
+        constexpr enum_bitmask operator|(enum_bitmask b) const
+        {
+            return mask() | b.mask();
+        }
+
+        constexpr enum_bitmask operator^(enum_bitmask b) const
+        {
+            return mask() ^ b.mask();
         }
 
         constexpr bool operator[](enum_bitmask e) const
         {
-            return static_cast<bool>(super::mask() & e.mask());
+            return static_cast<bool>(mask() & e.mask());
         }
 
         constexpr enum_bitmask& operator+=(enum_bitmask e)
         {
-            super::m_mask |= e.mask();
+            m_mask |= e.mask();
             return *this;
         }
 
         constexpr enum_bitmask& operator-=(enum_bitmask e)
         {
-            super::m_mask &= ~static_cast<Value>(e.mask());
+            m_mask &= ~static_cast<Value>(e.mask());
             return *this;
         }
 
         constexpr enum_bitmask& operator^=(enum_bitmask e)
         {
-            super::m_mask ^= static_cast<Value>(e.mask());
+            m_mask ^= static_cast<Value>(e.mask());
             return *this;
         }
-    };
 
-    // A bitset-like class for (scoped) enums defined as indices (i.e. 1, 2, 3, 4...)
-    template <typename Enum, typename Value = meta::underlying_type<Enum>>
-        requires std::is_enum_v<Enum>
-    class enum_bitset : public detail::enum_bit_base<Enum, Value>
-    {
+        constexpr bool operator<=>(const enum_bitmask&) const = default;
+
     private:
-        using super = detail::enum_bit_base<Enum, Value>;
-
-        constexpr static Value reduce(std::initializer_list<Enum> e)
-        {
-            Value mask { 0 };
-
-            for (const Enum value : e)
-                mask |= (1 << static_cast<Value>(value));
-
-            return mask;
-        }
-
-        constexpr static Value one { 1 };
-
-    public:
-        using super::super;
-
-        constexpr enum_bitset(Enum e)
-            : enum_bitset { e }
-        {
-        }
-
-        constexpr enum_bitset(std::initializer_list<Enum> il)
-            : super { reduce(il) }
-        {
-        }
-
-        constexpr bool operator[](enum_bitset e) const
-        {
-            return static_cast<bool>(super::mask() & e.mask());
-        }
-
-        constexpr enum_bitset& operator+=(enum_bitset e)
-        {
-            super::m_mask |= e.mask();
-            return *this;
-        }
-
-        constexpr enum_bitset& operator-=(enum_bitset e)
-        {
-            super::m_mask &= ~e.mask();
-            return *this;
-        }
-
-        constexpr enum_bitset& operator^=(enum_bitset e)
-        {
-            super::m_mask ^= e.mask();
-            return *this;
-        }
+        Value m_mask;
     };
 }
