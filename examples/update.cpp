a915931fd7103c9b297f4bb4c2e056f79419a71e
lyorig <31075205+lyorig@users.noreply.github.com>
2024-07-25 06:47:52 +0200

    added files

---
+++ examples/update.cpp
@@ -0,0 +1,78 @@
+#include <cstdlib>
+#include <thread>
+
+#include <halcyon/video.hpp>
+
+int main(int argc, char* argv[])
+{
+    static_assert(hal::meta::is_correct_main<main>);
+
+    hal::context       c;
+    hal::system::video v { c };
+
+    hal::window   wnd { v, "Example", { 640, 480 } };
+    hal::renderer rnd { wnd, hal::renderer::flag::vsync };
+
+    {
+        const hal::pixel::format pref { rnd.info().formats().front() };
+
+        hal::static_texture t { rnd, rnd.size(), pref };
+
+        hal::surface s { t.size(), pref };
+
+        s.fill(hal::palette::cyan);
+        t.update(s);
+
+        s.fill(hal::palette::orange);
+        t.update(s, t.size() / 2);
+
+        s.fill(hal::palette::green);
+        t.update(s, { { 0, 0 }, s.size() / 4 });
+
+        rnd.draw(t)();
+        rnd.present();
+    }
+
+    hal::event::holder h;
+
+    while (true)
+    {
+        while (v.events.poll(h))
+        {
+            switch (h.kind())
+            {
+                using enum hal::event::type;
+
+            case quit_requested:
+                return EXIT_SUCCESS;
+
+            case key_pressed:
+                switch (h.keyboard().button())
+                {
+                    using enum hal::keyboard::button;
+
+                case Q:
+                    break;
+
+                case W:
+                    break;
+
+                case E:
+                    break;
+
+                default:
+                    break;
+                }
+                break;
+
+            default:
+                break;
+            }
+        }
+
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(20ms);
+    }
+
+    return EXIT_FAILURE;
+}
