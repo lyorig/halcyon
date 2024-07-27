37bca4cc2c5f3cf33797b18b94a2a62d93899c4e
lyorig <31075205+lyorig@users.noreply.github.com>
2024-07-27 11:42:11 +0200

    actually, screw scripts

--- build.sh
+++
@@ -1,9 +0,0 @@
-if ! [ -f type.txt ]; then
-	echo "Build type file not found. Did you forget to run cfg.sh?"
-	exit 1
-fi
-
-type=$(<type.txt)
-
-echo "Building config ${type}"
-cmake --build build -j --config ${type}
